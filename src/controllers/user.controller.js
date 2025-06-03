ae836763db342b68e88cde4000065b64650deef2
Shruti Singh <shrutisinghskr@gmail.com>
2025-06-04 02:03:12 +0530

    Refactor user registration logic and enhance ApiResponse utility; update .gitignore and remove unused constants file

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,18 @@
+import ApiResponse from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import User from "../models/user.models.js"
 
 export const registerUser = asyncHandler( async ( req , res ) => {
-    res.status(200).json({
-        message : "User registered successfully",
-    })
+   
+   const { email, password } = req.body;
+   
+   if(!email || !password) {
+    return new ApiResponse(400, null, "Email and password are required").send(res);
+   }
+    
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+        return new ApiResponse(400, null, "User already exists").send(res);
+    }    
+    
 })
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,10 +1,12 @@
 class ApiResponse {
     constructor(statusCode, data, message = "Success") {
-        this.statusCode = statusCode;
-        this.data = data;
-        this.message = message;
-        this.success = statusCode < 400
+        this.statusCode = statusCode;  // HTTP status code (e.g., 200, 400)
+        this.data = data;              // The response payload
+        this.message = message;        // A success/failure message
+        this.success = statusCode < 400; // Determines if it’s a success
+    }
+    send(res) {
+        return res.status(this.statusCode).json(this);
     }
 }
-
-module.exports = ApiResponse;
+export default ApiResponse;
