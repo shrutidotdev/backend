d9f7268630d124f043153c72a2ad0bec302c9866
Shruti Singh <shrutisinghskr@gmail.com>
2025-06-10 01:53:43 +0530

    Add new user-related routes for password change, current user retrieval, account updates, avatar and cover image uploads, user channel profile access, and watch history retrieval in user router. Enhance existing logout and refresh token routes for improved functionality.

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import {
+    loginUser, logOutUser, refreshAccessToken, registerUser, changePassword,
+    getCurrentUser, updateAccountDetails, updateAvatar, updateCoverImage, getUserChannelProfile, getWatchHistory
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -22,8 +25,16 @@ router.route("/register").post(
 router.route("/login").post(loginUser)
 
 // secured route => verifyJwt middleware whicch runs because of next() to run forward to logOutUser
-router.route("/logout").post(verifyJwt , logOutUser)
+router.route("/logout").post(verifyJwt, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").patch(verifyJwt, changePassword)
+router.route("/current-user").get(verifyJwt, getCurrentUser)
+router.route("/update-account").patch(verifyJwt, updateAccountDetails)
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateAvatar)
+router.route("/coverImage").patch(verifyJwt, upload.single("coverImage"), updateCoverImage)
+// bcuz we have used req.params
+router.route("/c/:username").get(verifyJwt,getUserChannelProfile)
+router.route("/history").get(verifyJwt, getWatchHistory)
 export default router;
 
 
